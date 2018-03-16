cbc08bb94fe9b5b5a2a415e57adcb4cdc612f675
Chase Murray <cmurray3@buffalo.edu>
2018-03-16 13:13:06 -0400

    Added "R" installation

--- auto_install_1604.sh
+++ auto_install_1604.sh
@@ -52,7 +52,8 @@
 #			One exception:  We sudo pip to install virtualenv and virtualenvwrapper	
 #		Added pgRouting installation
 #		Modified version of Cesium -- FIXME
-
+#	3/16/18:
+#		Added R/RStudio
 
 set -e
 
@@ -103,6 +104,23 @@ sudo apt-get --yes install meld
 sudo apt-get --yes install kazam
 
 sudo apt-get update
+
+
+# R and RStudio:
+# See https://www.r-bloggers.com/how-to-install-r-on-linux-ubuntu-16-04-xenial-xerus/
+sudo echo "deb http://cran.rstudio.com/bin/linux/ubuntu $(lsb_release -sc)/" | sudo tee -a /etc/apt/sources.list
+gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
+gpg -a --export E084DAB9 | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install r-base r-base-dev
+sudo apt-get install gdebi-core
+
+# FIXME:  This is for 16.04 (xenial) only.
+# See https://www.rstudio.com/products/rstudio/download/#download for the latest versions of RStudio.
+wget https://download1.rstudio.org/rstudio-xenial-1.1.442-amd64.deb
+sudo gdebi -n rstudio-xenial-1.1.442-amd64.deb
+rm rstudio-xenial-1.1.442-amd64.deb
+
 
 
 # FIXME -- Should we remove (uninstall) old versions of pip?
